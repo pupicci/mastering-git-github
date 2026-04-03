7ef770bef7bc3ce8a85676fbe8d3b8e6b4317a5e
pupicci <tiim@nate.com>
2026-04-03 15:30:54 +0900

    Change the title of main page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '운영팀 웹 어드민 시스템' });
 });
 
 module.exports = router;
